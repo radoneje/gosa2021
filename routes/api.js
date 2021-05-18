eb439c3b6f7299b4e6f6d419241b2b04ca527cb0
radoneje <den.shevchenko@gmail.com>
2021-05-18 16:12:20 +0300

    h

--- routes/api.js
+++ routes/api.js
@@ -183,6 +183,7 @@ router.post('/newUser', function(req, res, next) {
 
 router.get('/users', function(req, res, next) {
   var txt="";
+  return res.send(dictonary);
   Object.keys(req.count).forEach(hall=>{
     Object.keys(req.count[hall]).forEach(lang=>{
       txt+=hall+"\t"+ (dictonary[hall])+"\t"+lang+"\t"+req.count[hall][lang] +"<br/>";
